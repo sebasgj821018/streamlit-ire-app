a25074c0599c4992082c5c28dfe7899834bbf66c
sebasgj <sebasgj@gmail.com>
2025-04-14 16:17:16 -0500

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -3,7 +3,7 @@ import yaml
 from yaml.loader import SafeLoader
 
 def autenticar_usuario():
-    with open('auth/config.yaml') as file:
+    with open('auth/config.yaml', encoding='utf-8') as file:
         config = yaml.load(file, Loader=SafeLoader)
 
     authenticator = stauth.Authenticate(
